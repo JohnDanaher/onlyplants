2bacb538de527d720231ceb7e4aa54e18bccded5
John <jayblez@gmail.com>
2023-01-09 10:52:20 +0100

    saving fanny hours

--- services/api.service.js
+++ services/api.service.js
@@ -1,26 +1,12 @@
 const axios = require('axios');
 // require('dotenv').config();
-// const myKey = 'GtwvOFPOA8yth1ZlegGDYzNUZWbm80Y-LHrNZXi318k'
-
-// class ApiService {
-//   constructor() {
-//     this.api = axios.create({
-//       baseURL: `https://trefle.io/api/v1/`
-//     });
-//   }
-
-// findPlantName(name){
-//     return this.api.get(`plants?token=${myKey}&filter%5Bcommon_name%5D=${name}`);
-
-// } 
-// };
-
+const {MYKEY} = process.env;
 class ApiService {
   constructor() {
     this.api = axios.create({
       baseURL: `https://house-plants.p.rapidapi.com/common`,
       headers: {
-        'X-RapidAPI-Key': 'ff98b136f1msh9608992364213e3p199f36jsn97d68b2dfb30',
+        'X-RapidAPI-Key': `${MYKEY}`,
         'X-RapidAPI-Host': 'house-plants.p.rapidapi.com'
       }
     });
