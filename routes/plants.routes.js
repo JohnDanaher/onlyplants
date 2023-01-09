dade1ec0340723713ab1422051da84270cc277d8
John <jayblez@gmail.com>
2023-01-09 16:02:09 +0100

    sorted most of the plant routes and model connections

--- routes/plants.routes.js
+++ routes/plants.routes.js
@@ -1,39 +1,58 @@
+const { isLoggedIn, isOwnProfile } = require('../middlewares/routes.guard');
 const express = require('express');
 const router = express.Router();
 const Plant = require("../models/Plant.model");
+const Room = require('../models/Room.model');
+const User = require('../models/User.model');
 const ApiService = require('../services/api.service');
 const apiService = new ApiService();
 
 
 router.get("/plants/create", (req, res) => {
-  res.render("plants/create");
+    Room.find()
+    .then(rooms => {
+  res.render("plants/create", {rooms});
+})
 });
 
 router.post("/plants/create", (req, res) => {
-    const {nickname, dob} = req.body;
-    apiService
-    .findPlant(req.body.name)
-    .then((result) => {
-        const details = result.data[0];
+    const {nickname, room} = req.body;
+    // apiService
+    // .findPlant(req.body.name)
+    // .then((result) => {
+    //     const details = result.data[0];
         Plant.create({
-            commonName: details.common[0],
+            // commonName: details.common[0],
             nickname: nickname,
-            // dob: dob, it doesn't seem to like this very much...
-            light: details.ideallight,
-            waterSchedule: details.watering,
-            minTemp: details.tempmin.celsius,
-            maxTemp: details.tempmax.celsius,
-            toleratedLight: details.toleratedlight,
-            latinName: details.latin
+            room: room,
+            parent: req.session.user.id
+            // light: details.ideallight,
+            // waterSchedule: details.watering,
+            // minTemp: details.tempmin.celsius,
+            // maxTemp: details.tempmax.celsius,
+            // toleratedLight: details.toleratedlight,
+            // latinName: details.latin
         })
         .then(newPlant => {
-            console.log(newPlant);
-        res.redirect(`/plants/details/${newPlant._id}`)
+            Room.findById(room)
+            .then(plantRoom => {
+                plantRoom.plants.push(newPlant);
+                plantRoom.save()
+                .then(() => {
+                    User.findById(req.session.user.id)
+                    .then(plantDaddy => {
+                        plantDaddy.plants.push(newPlant);
+                        plantDaddy.save()
+                    })
+                .then(() => res.redirect(`/plants/details/${newPlant._id}`)
+                )
+            })
+        })
     
         })
+        
     })
-});
-
+// });
 
 router.get("/plants/details/:id", (req, res) => {
     const {id} = req.params;
@@ -48,15 +67,23 @@ router.get("/plants/edit/:id", (req, res) => {
     const {id} = req.params;
     Plant.findById(id)
     .then((plant) => {
-        console.log(plant);
-        res.render("plants/edit", {plant})
+        Room.find()
+        .then((allRooms) => {
+        res.render("plants/edit", {plant, allRooms})
+    })
     })
 });
 
-// router.post("/plants/edit/:id", (req, res) => {
-//     const {id} = req.params;
+router.post("/plants/edit/:id", (req, res) => {
+    const {id} = req.params;
+    const {nickname, room} = req.body;
+    Plant.findByIdAndUpdate(id, {nickname, room})
+    .then(updatedPlant => {
+        console.log(updatedPlant);
+        res.redirect(`/plants/details/${id}`)
+    })
 
-// });
+});
 
 router.post("/plants/delete/:id", (req, res) => {
     const {id} = req.params;
--- services/api.service.js
+++ services/api.service.js
@@ -4,18 +4,19 @@ const {MYKEY} = process.env;
 class ApiService {
   constructor() {
     this.api = axios.create({
-      baseURL: `https://house-plants.p.rapidapi.com/common`,
-      headers: {
-        'X-RapidAPI-Key': `${MYKEY}`,
-        'X-RapidAPI-Host': 'house-plants.p.rapidapi.com'
-      }
-    });
-  }
+baseURL: `https://house-plants.p.rapidapi.com/common`,
+headers: {
+  'X-RapidAPI-Key': `${MYKEY}`,
+  'X-RapidAPI-Host': 'house-plants.p.rapidapi.com'
+}
+});
+}
 
 findPlant(name){
-    return this.api.get(`/${name}`);
+return this.api.get(`/${name}`);
 }
 
 };
 
 module.exports = ApiService;
+
